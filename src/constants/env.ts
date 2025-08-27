6513dabd47f13560e2a33b93e6cc4dc1532faa77
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-08-26 23:58:54 -0300

    ⚙️ config: update env validator

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -5,6 +5,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).or(z.literal('')).default('dev'),
   PORT: z.coerce.number().default(3333),
+  FRONTEND_DOMAIN: z.coerce.string().default('http://localhost:5173'),
 })
 
 const validation = envSchema.safeParse(process.env)
