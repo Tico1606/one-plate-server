1a056205c2dbcdf83f01d42be16c0a899bb70e17
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-10-06 01:20:35 -0300

    💾 database: add script to enable unaccent extension in PostgreSQL; update PrismaClient logging for test environment

--- src/database/prisma/prisma-client.ts
+++ src/database/prisma/prisma-client.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from '@prisma/client'
 import { env } from '@/constants/env.ts'
 
 export const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+  log: (env.NODE_ENV === 'dev' || env.NODE_ENV === 'test') ? ['query'] : [],
 })
